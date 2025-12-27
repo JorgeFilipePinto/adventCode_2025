460e9d2a70b31971f38d01f3a409960398b164c7
Jorge Pinto <jorge.filipepinto@hotmail.com>
2025-12-27 15:22:13 +0000

    Day 2 finish

--- Day2/chalangeTwo/chalangeTwo.py
+++ Day2/chalangeTwo/chalangeTwo.py
@@ -31,18 +31,19 @@ def getInvalidIDs(inputData):
         idRange = ids.split("-")
         min = idRange[0]
         max = idRange[1]
+        print(f"min: {min} - max: {max}")
         counter = int(min)
+        invalidIDsList = []
         
         while counter <= int(max):
             strCounter = str(counter)
             halfSize = len(strCounter) / 2
-            isInvalid = False
-            
-            if len(strCounter) % 2 == 0:
+            isInvalid = everyNumberAreEqual(strCounter)
+
+            if len(strCounter) % 2 == 0 and not isInvalid:
                 if strCounter[:int(len(strCounter) / 2)] == strCounter[int(len(strCounter) / 2):]:
                     isInvalid = True
                 elif strCounter[:2] == strCounter[len(strCounter) - 2:len(strCounter)] and int(len(strCounter) / 2) > 1:
-                    print(f"{strCounter}: {strCounter[:2]} ---->{strCounter[len(strCounter) - 2:len(strCounter)]}")
                     numberOfParts = int(len(strCounter) / 2)
                     parts = []
                     for i in range(0, numberOfParts + 2, 2):
@@ -55,23 +56,36 @@ def getInvalidIDs(inputData):
                             break
                         else:
                             isInvalid = True
-            else:
-                for i in range(len(strCounter)-1):
-                    if strCounter[i] == strCounter[i + 1]:
-                        isInvalid = True
-                    else:
-                        isInvalid = False
-                        break
+            elif len(strCounter) % 3 == 0 and len(strCounter) > 1 and not isInvalid:
+                if len(strCounter) > 5:
+                    numberOfParts = int(len(strCounter) / 3)
+                    parts = []
+                    for i in range(0, numberOfParts + 6, 3):
+                        part = strCounter[i:i + 3]
+                        parts.append(part)
 
+                    isInvalid = True
+                    for i in range(len(parts)-1):
+                        if parts[i] != parts[i + 1]:
+                            isInvalid = False
 
             if isInvalid:
                 invalidIDs += counter
-                print(f"Invalid ID: {counter}")
-            counter += 1
+                invalidIDsList.append(str(counter))
+                #print(f"Invalid ID: {counter}")
 
+            counter += 1
     return invalidIDs
 
 
+def everyNumberAreEqual(strNumber):
+    if len(strNumber) > 1:
+        for i in range(len(strNumber)-1):
+            if strNumber[i] != strNumber[i +1]:
+                return False
+        return True
+    return False
+
 
 inputs = getInputs("inputs.txt")
-print(f"We have {getInvalidIDs(test)} invalid IDs")
+print(f"We have {getInvalidIDs(inputs)} invalid IDs")
